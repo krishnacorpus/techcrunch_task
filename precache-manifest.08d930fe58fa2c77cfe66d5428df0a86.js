5dd84534513ec06b14a3250cd5e540c25f37ae11
krishnacorpus <krishna.kondapalli@imversion.com>
2020-06-02 12:21:57 +0530

    Updates

---
+++ precache-manifest.08d930fe58fa2c77cfe66d5428df0a86.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3ad16433adcca65d98fcda7d3c7aa7e9",
+    "url": "/techcrunch_task/index.html"
+  },
+  {
+    "revision": "097bc0f268f6291811ef",
+    "url": "/techcrunch_task/static/css/main.293214ee.chunk.css"
+  },
+  {
+    "revision": "4f05b031dc52b2a6e450",
+    "url": "/techcrunch_task/static/js/2.d4c75dbc.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/techcrunch_task/static/js/2.d4c75dbc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "097bc0f268f6291811ef",
+    "url": "/techcrunch_task/static/js/main.366c29c8.chunk.js"
+  },
+  {
+    "revision": "066cd82bc3a747d31ba8",
+    "url": "/techcrunch_task/static/js/runtime-main.5fb72a75.js"
+  }
+]);
